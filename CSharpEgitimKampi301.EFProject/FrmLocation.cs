33a1a915358f5ed5e9938c1c0b4eed00729a7bb0
Elif Sülün <84282621+elifsulun@users.noreply.github.com>
2025-06-01 14:21:25 +0300

    Add files via upload

---
+++ CSharpEgitimKampi301.EFProject/FrmLocation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+        }
+
+        EgitimKampiEFTravelDbEntities db = new EgitimKampiEFTravelDbEntities();
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.Location.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void FrmLocation_Load(object sender, EventArgs e)
+        {
+            var values = db.Guide.Select(x => new
+            {
+                FullName = x.GuideName + " " + x.GuideSurname,
+                x.GuideId
+            }).ToList();
+
+            cmbGuide.DisplayMember = "FullName";
+            cmbGuide.ValueMember = "GuideId";
+            cmbGuide.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Location location = new Location();
+            location.Capacity = byte.Parse(nudCapacity.Value.ToString());
+            location.City = txtCity.Text;
+            location.Country = txtCountry.Text;
+            location.Price = decimal.Parse(txtPrice.Text);
+            location.DayNight = txtDayNight.Text;
+            location.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+            db.Location.Add(location);
+            db.SaveChanges();
+            MessageBox.Show("Ekleme işlemi başarılı");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var value = db.Location.Find(id);
+            if (value != null)
+            {
+                db.Location.Remove(value);
+                db.SaveChanges();
+                MessageBox.Show("Silme işlemi başarılı");
+            }
+            else
+            {
+                MessageBox.Show("Silinecek kayıt bulunamadı");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updatedValue = db.Location.Find(id);
+            updatedValue.DayNight = txtDayNight.Text;
+            updatedValue.City = txtCity.Text;
+            updatedValue.Country = txtCountry.Text;
+            updatedValue.Capacity = byte.Parse(nudCapacity.Value.ToString());
+            updatedValue.Price = decimal.Parse(txtPrice.Text);
+            updatedValue.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+            db.SaveChanges();
+            MessageBox.Show("Güncelleme işlemi başarılı");
+        }
+    }
+}
